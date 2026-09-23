20aaa9af015eaf03b6b88b3b0f6971127d41bfb4
wdbkzsq <1638701678@qq.com>
2026-09-23 14:21:41 +0000

    util lab: add sixfive command

--- Makefile
+++ Makefile
@@ -151,6 +151,7 @@ UPROGS=\
 	$U/_dorphan\
 	$U/_sync\
 	$U/_sleep\
+	$U/_sixfive\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/sixfive.c
@@ -0,0 +1,48 @@
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+void
+sixfive(int fd)
+{
+  char buf[128];
+  char div[9] = {' ', '-', '\r', '\t', '\n', '.', '/', ',', '\0'};
+  int flag = 1, i, sum = 0;
+  int n = read(fd, buf, sizeof(buf) - 1);
+  for (i = 0; i < n; i++) {
+    if (buf[i] >= '0' && buf[i] <= '9' && flag == 1) {
+      sum = sum * 10 + buf[i] - '0';
+      if (i == n - 1) {
+        printf("%d\n", sum);
+      }
+    } else if (strchr(div, buf[i])) {
+      if ((sum % 6 == 0 || sum % 5 == 0) && sum != 0 && flag == 1) {
+        printf("%d\n", sum);
+      }
+      sum = 0;
+      flag = 1;
+    } else {
+      flag = 0;
+    }
+  }
+}
+
+int
+main(int argc, char **argv)
+{
+  int i, fd;
+  if (argc < 2) {
+    sixfive(0);
+    exit(0);
+  }
+
+  for (i = 1; i < argc; i++) {
+    if ((fd = open(argv[i], O_RDONLY)) < 0) {
+      fprintf(2, "sixfive: cannot open %s\n", argv[i]);
+      exit(1);
+    }
+    sixfive(fd);
+    close(fd);
+  }
+  exit(0);
+}
